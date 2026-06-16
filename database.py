80ee54051ab99f9fe67c49c5846d051ce218e49d
ytran98161200-sketch <ytran98161200@gmail.com>
2026-06-16 10:09:40 +0700

    Fix Streamlit Secrets

--- database.py
+++ database.py
@@ -1,11 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+import streamlit as st
 import os
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    DATABASE_URL = st.secrets["DATABASE_URL"]
 
 engine = create_engine(
     DATABASE_URL,
